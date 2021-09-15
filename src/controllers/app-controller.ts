93e920d72197f33f8595fb5f43285ab7bf9c0d2e
Alexandre Borges <alexandre.bsilva55@gmail.com>
2021-09-15 20:21:59 -0300

    adding info to health route

--- src/controllers/app-controller.ts
+++ src/controllers/app-controller.ts
@@ -1,9 +1,14 @@
 import { JsonController, Get } from "routing-controllers";
+import { App } from "../app";
 
 @JsonController()
 export class UserController {
   @Get("/health")
   getAll() {
-    return { success: true, message: "Api ok!" };
+    return {
+      success: true,
+      message: "Api ok!",
+      startedServerAt: App.startedServerAt,
+    };
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import "reflect-metadata";
 import path from "path";
 
 import { createExpressServer } from "routing-controllers";
+import { App } from "./app";
 
 // creates express app, registers all controller routes and returns you express app instance
 const app = createExpressServer({
   controllers: [path.join(__dirname + "/controllers/*")], // we specify controllers we want to use
 });
 
-// run express application on port 3000
-app.listen(process.env.PORT ?? 3000, () => {
-  console.log("Api running on port 3000");
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () => {
+  console.log(`Api running on port ${port}`);
+  App.startedServerAt = new Date();
 });
